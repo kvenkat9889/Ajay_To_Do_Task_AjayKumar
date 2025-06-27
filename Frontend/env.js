7c80780832018f9224ddd1186e090fe0c3cdb448
kvenkat9889 <kvenkat9889@gmail.com>
2025-06-27 11:20:53 +0530

    updated

--- Frontend/env.js
+++ Frontend/env.js
@@ -1,1 +1,1 @@
-window.API_BASE_URL = window.location.hostname === '44.223.23.145' ? 'http://backend:3013/api' : 'http://44.223.23.145:3423/api';
+window.API_BASE_URL = window.location.hostname === '44.223.23.145' ? 'http://backend:3423/api' : 'http://44.223.23.145:3423/api';
